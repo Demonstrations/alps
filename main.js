c3aa5b88af941599d8b4ff0180b778fec7eabc8c
Y's men <honkyhut@163.com>
2017-08-24 22:09:47 +0800

    修改注释

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const { spawn } = require('child_process');
 const child = spawn(require('electron'), 
                     ['./src/electron/index.js'], 
                     {stdio:['ipc']});
-//
+//接收electron子进程发过来的消息
 child.on("message", (m) => {
     console.log(`接收到的消息：${m.a}`)
 });
-//
+//监听进程是否关闭
 child.on('close', (code, signal) => {
     console.log(`程序已退出：${signal}`);
 });
