a82e70db5a92bf502da27c757a251731a30485b2
胡汉权 <hanquan.hhq@alibaba-inc.com>
2017-08-29 11:49:19 +0800

    加入electron-builder依赖包

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
 'use strict'
-//load config
-const fs = require("fs");
-const config = JSON.parse(fs.readFileSync("./config.json", {encoding:"utf8"}));
-// console.log(config.remoteHost);
-const { spawn } = require('child_process');
-//electron process
-const child = spawn(require('electron'), 
-                    ['./src/electron/index.js'], 
-                    {stdio:['ipc'], argv0:config.remoteHost});
-//接收electron子进程发过来的消息
-child.on("message", (m) => {
-    console.log(`接收到的消息：${m}`)
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow = null;
+//远程页面连接地址
+let remoteUrl = require('./config.json').remoteHost;
+
+function createWindow () {
+  if(!remoteUrl)return;
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(remoteUrl, 'index.html'),
+    protocol: 'http:',
+    slashes: true
+  }));
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
-//监听进程是否关闭
-child.on('close', (code, signal) => {
-    console.log(`程序已退出：${signal}`);
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
